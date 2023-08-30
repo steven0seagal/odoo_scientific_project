8033a3715b5b66339e632380a1e4d67cf4706296
bartek <bart@genotic.com>
2023-08-30 12:29:49 +0200

    tracking project

--- odoo/addons/scientific_project/models/project.py
+++ odoo/addons/scientific_project/models/project.py
@@ -3,18 +3,19 @@ from odoo import models, fields
 class Project(models.Model):
     _name = 'scientific.project'
     _description = 'Scientific Project'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string='Name', required=True)
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
-    description = fields.Text(string='Description')
+    description = fields.Text(string='Description',tracking=True)
     status = fields.Selection([
         ('draft', 'Draft'),
         ('in_progress', 'In Progress'),
         ('done', 'Done'),
         ('cancelled', 'Cancelled'),
-    ], string='Status', default='draft')
-    document_id = fields.Many2one('scientific.document', string='Document')
+    ], string='Status', default='draft',tracking=True)
+    document_id = fields.Many2one('scientific.document', string='Document', tracking=True)
     funding = fields.Many2many('scientific.funding', string='Funding')
     principal_investigator_id = fields.Many2one('scientific.researcher', string='Principal Investigator')
     collaborators_ids = fields.Many2many('res.partner', string='Collaborators')
