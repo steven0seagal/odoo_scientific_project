952fb0a944c942bef8127c7d3dc5e39d5a23087c
bartek <bart@genotic.com>
2023-10-01 00:44:30 +0200

    experiment view

--- odoo/addons/scientific_project/models/experiment.py
+++ odoo/addons/scientific_project/models/experiment.py
@@ -5,15 +5,17 @@ class ScientificExperiment(models.Model):
     _description = 'Experiment'
 
     name = fields.Char(string='Name', required=True)
-    notes = fields.Text(string='Description')
+    description = fields.Text(string='Description')
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
     status = fields.Selection([('planning', 'Planning'), ('in_progress', 'In Progress'), ('completed', 'Completed')], string='Status')
-    project_id = fields.Many2many('scientific.project', string='Project')
+    project_id = fields.Many2one('scientific.project', string='Project')
     introduction = fields.Text(string='Introduction')
     hypothesis = fields.Text(string='Hypothesis')
     results = fields.Text(string='Results')
     conclusion = fields.Text(string='Conclusion')
     methodology = fields.Text(string='Methodology')
     raport_created = fields.Boolean(string='Raport Created',default=False)
-
+    notes = fields.Text(string='Notes')
+    document_id = fields.Many2many('scientific.document', string='Document')
+    assigned_to_ids = fields.Many2many('scientific.researcher', string='Assigned to')
